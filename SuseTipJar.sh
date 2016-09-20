f58d9a9568e47d4f4eaa381644c4b41a562ff016
pigeonlips <pigeonlips@gmail.com>
2016-09-20 12:29:26 +1200

    updates

--- SuseTipJar.sh
+++ SuseTipJar.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-TIPJAR=()
+function tipjar() {
+  TIPJAR=()
 
-# add your tips here v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
+  # add your tips here v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v v
 
-TIPJAR+=("What info ? \033[0;32mjournalctl -b\033[0m to show the journal since last boot ...")
-TIPJAR+=("You should run \033[0;32msudo zypper up\033[0m to refresh repos and update the system ...")
-TIPJAR+=("Run this \033[0;32msudo snapper create --description 'Uh-Oh'\033[0m before making any big changes ..!")
+  TIPJAR+=("What info ? \033[0;32mjournalctl -b\033[0m to show the journal since last boot ...")
+  TIPJAR+=("You should run \033[0;32msudo zypper up\033[0m to refresh repos and update the system ...")
+  TIPJAR+=("Run this \033[0;32msudo snapper create --description 'Uh-Oh'\033[0m before making any big changes ..!")
 
-# add your tips here ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+  # add your tips here ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
 
-# Select a random tip
-TIP=${TIPJAR[RANDOM%${#TIPJAR[@]}]}
+  # Select a random tip
+  TIP=${TIPJAR[RANDOM%${#TIPJAR[@]}]}
 
-# DONE we should check to see if cowsay is installed
-command -v cowsay >/dev/null 2>&1 || { echo -e "${TIP}"; exit 1; }
+  # DONE we should check to see if cowsay is installed
+  command -v cowsay >/dev/null 2>&1 || { echo -e "${TIP}"; exit 1; }
 
-# DONE Create cow file on the fly
+  # DONE Create cow file on the fly
+  COWFILE=`mktemp`
 echo '$the_cow = <<"EOC";
         $thoughts    ____
          $thoughts  /@   `~-..
@@ -24,27 +26,29 @@ echo '$the_cow = <<"EOC";
              // //   /
              ^  ^   @
 
-EOC' > $HOME/susetip.cow
+EOC' > $COWFILE
 
-## get the number of lines in file
-#wc -l < ~/test.txt
+  ## get the number of lines in file
+  #wc -l < ~/test.txt
 
-## pipe text from file with color to cowsay
-#echo -e "$(tail ~/test.txt )" | cowsay
+  ## pipe text from file with color to cowsay
+  #echo -e "$(tail ~/test.txt )" | cowsay
 
-## get forth line from file
-#sed "4q;d" ~/test.txt
+  ## get forth line from file
+  #sed "4q;d" ~/test.txt
 
-## get a random number 1 through 10
-#$(( ( RANDOM % 10 )  + 1 ))
+  ## get a random number 1 through 10
+  #$(( ( RANDOM % 10 )  + 1 ))
 
-# putting it all together
-# echo -e `sed "$(( ( RANDOM % \`wc -l < ~/test.txt\` )  + 1 ))q;d" ~/test.txt ` | cowsay
+  # putting it all together
+  # echo -e `sed "$(( ( RANDOM % \`wc -l < ~/test.txt\` )  + 1 ))q;d" ~/test.txt ` | cowsay
 
-# do the work, show a tip
-printf "%b " $TIP | cowthink -f $HOME/susetip.cow -W${COLUMNS:-$(tput cols)} | sed -e "s/(/ /g" -e "s/)//g" -e "s/_/-/g"
+  # do the work, show a tip
+  printf "%b " $TIP | cowthink -f $COWFILE -W${COLUMNS:-$(tput cols)} | sed -e "s/(/ /g" -e "s/)//g" -e "s/_/-/g"
 
-# tidy up
-rm $HOME/susetip.cow
-unset TIP
-unset TIPS
+  # tidy up
+  rm $COWFILE
+  unset TIP
+  unset TIPS
+
+}
